d6305a8054feabe06ade6df0a3c54125f9a5ff58
Lotte Pitcher <github@lottepitcher.co.uk>
2024-10-15 22:30:27 +0100

    add custom property editor that display SUBSCRIBED status for date time property

---
+++ FastTrack.v13/App_Plugins/BackofficeCustomisation/DatePickerWithStatus/datepickerwithstatus.controller.js
@@ -0,0 +1,63 @@
+(function() {
+
+    function datePickerWithStatusController($scope) {
+
+        var vm = this;
+        vm.dateValue = "";
+        vm.dateStatus = "";
+
+        if ($scope.model.value) {
+            console.log("scope.model.value: " + $scope.model.value);
+            var convertedDate = convertUmbracoDateForDatePicker($scope.model.value);
+            console.log("convertedDate: " + convertedDate);
+            vm.dateValue = convertedDate;
+        }
+
+        setStatusForDate();
+
+        $scope.$on("formSubmitting", function (ev, args) {
+            $scope.model.value = vm.dateValue;
+            setStatusForDate();
+        });
+
+        function setStatusForDate() {
+            if (vm.dateValue) {
+                var dateObj = new Date(vm.dateValue);
+                if (dateObj > (new Date())) {
+                    vm.dateStatus = "ACTIVE";
+                }
+                else {
+                    vm.dateStatus = "EXPIRED";
+                }
+            }
+            else {
+                vm.dateStatus = "UNSUBSCRIBED";
+            }
+        }
+
+        function convertUmbracoDateForDatePicker(dateFromUmbraco) {
+
+            // ALL BACKOFFICE USERS ARE ENGLISH UK (NOT US) SO THIS APPROACH IS SAFE!
+
+            if (!dateFromUmbraco) return null;
+
+            var arrDateParts = dateFromUmbraco.split(" ")[0].split("/");
+            var dateDD = arrDateParts[0];
+            var dateMM = arrDateParts[1];
+            var dateYYYY = arrDateParts[2];
+
+            if (dateDD.length === 1) dateDD = "0" + dateDD;
+            if (dateMM.length === 1) dateMM = "0" + dateMM;
+            if (dateYYYY.length === 2) dateYYYY = "20" + dateYYYY;
+
+            return new Date(dateYYYY + "-" + dateMM + "-" + dateDD + "T00:00:00Z");
+        }
+
+        $scope.$watch('vm.dateValue', function (newval, oldval) {
+            setStatusForDate();
+        });
+    }
+
+    angular.module("umbraco").controller("Custom.DatePickerWithStatusController", datePickerWithStatusController);
+
+})();
