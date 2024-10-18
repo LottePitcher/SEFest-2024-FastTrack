ed769e1fbd8bb3e66e3da15207748ae6075a8813
Niels Lyngsø <niels.lyngso@gmail.com>
2024-10-18 21:47:19 +0200

    add security to controller

--- FastTrack.v15/SubscriptionsApi/Controllers/ListingApiController.cs
+++ FastTrack.v15/SubscriptionsApi/Controllers/ListingApiController.cs
@@ -1,15 +1,19 @@
 using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
 using FastTrack.v15.SubscriptionsApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Web.Common.Routing;
+using Umbraco.Cms.Web.Common.Authorization;
 
 namespace FastTrack.v15.SubscriptionsApi.Controllers;
 
 [ApiController]
+[Authorize(Policy = AuthorizationPolicies.BackOfficeAccess)]
 [ApiVersion("1.0")]
 [ApiExplorerSettings(GroupName = "Subscriptions API")]
 [BackOfficeRoute("subscriptions/api/v{version:apiVersion}")]
+[Produces("application/json")]
 public class ListingApiController : ControllerBase
 {
     private readonly IMemberService _memberService;
@@ -30,7 +34,7 @@ public class ListingApiController : ControllerBase
             .ThenBy(x => x.FirstName)
             .ToList();
     }
-    
+
     [HttpGet("GetExpired")]
     [ProducesResponseType(typeof(List<SubscriptionListItem>), StatusCodes.Status200OK)]
     public List<SubscriptionListItem> GetExpired()
@@ -42,7 +46,7 @@ public class ListingApiController : ControllerBase
             .ThenBy(x => x.FirstName)
             .ToList();
     }
-    
+
     [HttpGet("GetNonsubscribed")]
     [ProducesResponseType(typeof(List<SubscriptionListItem>), StatusCodes.Status200OK)]
     public List<SubscriptionListItem> GetNonsubscribed()
@@ -53,23 +57,23 @@ public class ListingApiController : ControllerBase
             .ThenBy(x => x.FirstName)
             .ToList();
     }
-    
+
     private List<SubscriptionListItem> GetAllMembers()
     {
         // NB for a performant real-world solution, should put custom properties in Examine index and filter using Examine 
-        
+
         var allMembers = _memberService.GetAll(0, int.MaxValue, out var totalRecords);
-        
+
         var subscriptions = allMembers.Select(x => new SubscriptionListItem
-            {
-                MemberKey = x.Key,
-                FirstName = x.GetValue<string>("firstName"),
-                LastName = x.GetValue<string>("lastName"),
-                Email = x.Email,
-                SubscriptionExpiry = x.GetValue<DateTime?>("subscriptionExpiry")
-            })
+        {
+            MemberKey = x.Key,
+            FirstName = x.GetValue<string>("firstName"),
+            LastName = x.GetValue<string>("lastName"),
+            Email = x.Email,
+            SubscriptionExpiry = x.GetValue<DateTime?>("subscriptionExpiry")
+        })
             .ToList();
-        
+
         return subscriptions;
     }
 }
