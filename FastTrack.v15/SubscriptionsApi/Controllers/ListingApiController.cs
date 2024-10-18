08b6b9f957dda9b2c4946ca65f9f83a1cb19eaae
Lotte Pitcher <github@lottepitcher.co.uk>
2024-10-18 16:04:28 +0100

    api endpoints working in 'default' definition

---
+++ FastTrack.v15/SubscriptionsApi/Controllers/ListingApiController.cs
@@ -0,0 +1,75 @@
+using Asp.Versioning;
+using FastTrack.v15.SubscriptionsApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Web.Common.Routing;
+
+namespace FastTrack.v15.SubscriptionsApi.Controllers;
+
+[ApiController]
+[ApiVersion("1.0")]
+[ApiExplorerSettings(GroupName = "Subscriptions API")]
+[BackOfficeRoute("subscriptions/api/v{version:apiVersion}")]
+public class ListingApiController : ControllerBase
+{
+    private readonly IMemberService _memberService;
+
+    public ListingApiController(IMemberService memberService)
+    {
+        _memberService = memberService;
+    }
+
+    [HttpGet("GetActive")]
+    [ProducesResponseType(typeof(List<SubscriptionListItem>), StatusCodes.Status200OK)]
+    public List<SubscriptionListItem> GetActive()
+    {
+        return GetAllMembers()
+            .Where(x => x.SubscriptionExpiry.HasValue && x.SubscriptionExpiry.Value >= DateTime.Now)
+            .OrderBy(x => x.SubscriptionExpiry)
+            .ThenBy(x => x.LastName)
+            .ThenBy(x => x.FirstName)
+            .ToList();
+    }
+    
+    [HttpGet("GetExpired")]
+    [ProducesResponseType(typeof(List<SubscriptionListItem>), StatusCodes.Status200OK)]
+    public List<SubscriptionListItem> GetExpired()
+    {
+        return GetAllMembers()
+            .Where(x => x.SubscriptionExpiry.HasValue && x.SubscriptionExpiry.Value < DateTime.Now)
+            .OrderBy(x => x.SubscriptionExpiry)
+            .ThenBy(x => x.LastName)
+            .ThenBy(x => x.FirstName)
+            .ToList();
+    }
+    
+    [HttpGet("GetNonsubscribed")]
+    [ProducesResponseType(typeof(List<SubscriptionListItem>), StatusCodes.Status200OK)]
+    public List<SubscriptionListItem> GetNonsubscribed()
+    {
+        return GetAllMembers()
+            .Where(x => !x.SubscriptionExpiry.HasValue)
+            .OrderBy(x => x.LastName)
+            .ThenBy(x => x.FirstName)
+            .ToList();
+    }
+    
+    private List<SubscriptionListItem> GetAllMembers()
+    {
+        // NB for a performant real-world solution, should put custom properties in Examine index and filter using Examine 
+        
+        var allMembers = _memberService.GetAll(0, int.MaxValue, out var totalRecords);
+        
+        var subscriptions = allMembers.Select(x => new SubscriptionListItem
+            {
+                MemberKey = x.Key,
+                FirstName = x.GetValue<string>("firstName"),
+                LastName = x.GetValue<string>("lastName"),
+                Email = x.Email,
+                SubscriptionExpiry = x.GetValue<DateTime?>("subscriptionExpiry")
+            })
+            .ToList();
+        
+        return subscriptions;
+    }
+}
