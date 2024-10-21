9ad7573009453e89235b83c4e3e7d3d0ae1aef93
Niels Lyngsø <niels.lyngso@gmail.com>
2024-10-21 09:36:05 +0200

    minor clean up of api setup

--- FastTrack.v15.Customizations.Frontend/src/api-setup.ts
+++ FastTrack.v15.Customizations.Frontend/src/api-setup.ts
@@ -3,10 +3,10 @@ import { UMB_AUTH_CONTEXT } from '@umbraco-cms/backoffice/auth';
 import { OpenAPI } from './api/index.ts';
 
 export const onInit: UmbEntryPointOnInit = async (host) => {
-	host.consumeContext(UMB_AUTH_CONTEXT, async (auth) => {
-		if (!auth) return;
+	host.consumeContext(UMB_AUTH_CONTEXT, async (authContext) => {
+		if (!authContext) return;
 
-		const umbOpenApi = auth.getOpenApiConfiguration();
+		const umbOpenApi = authContext.getOpenApiConfiguration();
 		OpenAPI.BASE = umbOpenApi.base;
 		OpenAPI.TOKEN = umbOpenApi.token;
 		OpenAPI.WITH_CREDENTIALS = umbOpenApi.withCredentials;
--- FastTrack.v15/App_Plugins/FastTrackCustomizations/api-setup.js
+++ FastTrack.v15/App_Plugins/FastTrackCustomizations/api-setup.js
@@ -1,9 +1,9 @@
 import { UMB_AUTH_CONTEXT as i } from "@umbraco-cms/backoffice/auth";
 import { O as e } from "./OpenAPI-CppMC34i.js";
-const p = async (o) => {
-  o.consumeContext(i, async (t) => {
-    if (!t) return;
-    const n = t.getOpenApiConfiguration();
+const p = async (t) => {
+  t.consumeContext(i, async (o) => {
+    if (!o) return;
+    const n = o.getOpenApiConfiguration();
     e.BASE = n.base, e.TOKEN = n.token, e.WITH_CREDENTIALS = n.withCredentials, e.CREDENTIALS = n.credentials;
   });
 };
